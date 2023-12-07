b66bac72bf215c3a0f5df2173b43561167988c4b
LevitatingBusinessMan (Rein Fernhout) <me@levitati.ng>
2023-12-07 17:19:37 +0100

    day 7 part 2

--- 7-rb/7.rb
+++ 7-rb/7.rb
@@ -1,7 +1,6 @@
-order = [:five_of_a_kind, :four_of_a_kind, :full_house, :three_of_a_kind, :two_pair, :one_pair, :high_card]
-
 def card_sort a, b
-  card_order = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]
+  card_order = if $part2 then ["A", "K", "Q", "T", "9", "8", "7", "6", "5", "4", "3", "2", "J"]
+	else ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"] end
   if card_order.index(a.chars.first) == card_order.index(b.chars.first)
     card_sort a[1..], b[1..]
   else
@@ -10,6 +9,7 @@ def card_sort a, b
 end
 
 def identify_hand hand
+	hand = hand.gsub /J/, hand.gsub(/JJJJJ/, "A").chars.filter{|c|c!="J"}.max_by { |i| hand.count(i) } if $part2
 	case hand.chars.uniq.length
 	when 1
 		return :five_of_a_kind
@@ -30,10 +30,17 @@ def identify_hand hand
 	end
 end
 
-p File.read("input").lines.map(&:split).map{|h,b|[h,b.to_i]}.sort! { |(hand1, bid1), (hand2, bid2)|
-	if order.index(identify_hand hand1) == order.index(identify_hand hand2)
-		card_sort hand1, hand2
-	else
-		order.index(identify_hand hand1) <=> order.index(identify_hand hand2)
-	end
-}.reverse.each.with_index(1).reduce(0) { |acc, ((hand, bid), i)| acc + bid * i }
+def run
+	order = [:five_of_a_kind, :four_of_a_kind, :full_house, :three_of_a_kind, :two_pair, :one_pair, :high_card]
+	File.read("input").lines.map(&:split).map{|h,b|[h,b.to_i]}.sort! { |(hand1, bid1), (hand2, bid2)|
+		if order.index(identify_hand hand1) == order.index(identify_hand hand2)
+			card_sort hand1, hand2
+		else
+			order.index(identify_hand hand1) <=> order.index(identify_hand hand2)
+		end
+	}.reverse.each.with_index(1).reduce(0) { |acc, ((hand, bid), i)| acc + bid * i }
+end
+
+puts "Part 1: #{run}"
+$part2 = true
+puts "Part 2: #{run}"
